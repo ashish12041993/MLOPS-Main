ae7058033db84da887be81685a37f831c6fc95ee
ashish12041993 <ashish.mnnit777@gmail.com>
2025-09-01 11:44:33 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ except Exception as e:
 
 @app.route("/")
 def index():
-    return "Welcome to the Flask ML App from Ashish multicloud Engineer with DevOPs🎉"
+    return "Welcome to the Flask ML App from Ashish mult-icloud Engineer with DevOPs🎉"
 
 @app.route("/predict", methods=["GET"])
 def predict():
